f4fa6a08a571082dba3483f425e87345b8944b42
Nama Kamu <emailkamu@gmail.com>
2026-04-11 19:38:41 +0700

    class buku command

--- Buku.java
+++ Buku.java
@@ -1,14 +1,18 @@
 public class Buku {
+    // properti Buku: kode buku, judul buku, dan tahun terbit
     String kodeBuku;
     String judul;
     int tahunTerbit;
 
+    // konstruktor untuk membuat objek Buku
     public Buku(String kodeBuku, String judul, int tahunTerbit) {
         this.kodeBuku = kodeBuku;
         this.judul = judul;
         this.tahunTerbit = tahunTerbit;
     }
+
+    // tampilkan data buku dalam format yang mudah dibaca
     public void tampil() {
-        System.out.println("Buku: " + kodeBuku + " | Judul: " + judul + " | Tahun: " + tahunTerbit);
+        System.out.println("Kode Buku: " + kodeBuku + " | Judul: " + judul + " | Tahun Terbit: " + tahunTerbit);
     }
 }
