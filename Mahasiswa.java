6d1cb6b25502f6618cfc518e40a7f918dbb677f4
Nama Kamu <emailkamu@gmail.com>
2026-04-11 19:38:58 +0700

    class mahasiswa command

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,12 +1,17 @@
 public class Mahasiswa {
+    // properti Mahasiswa: NIM, nama, dan program studi
     String nim;
     String nama;
     String prodi;
+
+    // konstruktor untuk membuat objek Mahasiswa
     public Mahasiswa(String nim, String nama, String prodi) {
         this.nim = nim;
         this.nama = nama;
         this.prodi = prodi;
     }
+
+    // tampilkan data mahasiswa dalam format yang jelas
     public void tampil() {
         System.out.println("NIM: " + nim + " | Nama: " + nama + " | Prodi: " + prodi);
     }
