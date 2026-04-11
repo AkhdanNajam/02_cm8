a8d5dc8b897a59581d02285ce9510c7665bce469
Nama Kamu <emailkamu@gmail.com>
2026-04-11 19:39:17 +0700

    class peminjaman command

--- Peminjaman.java
+++ Peminjaman.java
@@ -14,6 +14,8 @@ public class Peminjaman {
     }
 
     public void hitungDenda() {
+        // jika durasi pinjam lebih dari 5 hari, hitung hari terlambat
+        // lalu kalikan dengan besaran denda per hari
         if (lamaPinjam > 5) {
             terlambat = lamaPinjam - 5;
             denda = terlambat * 2000;
@@ -24,6 +26,7 @@ public class Peminjaman {
     }
 
     public void tampil() {
+        // tampilkan data peminjaman dalam format yang mudah dibaca
         System.out.println(mhs.nama + " | " + buku.judul +
                 " | Lama: " + lamaPinjam +
                 " | Terlambat: " + terlambat +
